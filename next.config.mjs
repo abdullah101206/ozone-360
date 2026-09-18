c014c7119e154e74dd1decf8dc215fc62c879c7e
abdullah101206 <abdullah101206@gmail.com>
2026-09-18 18:01:02 +0500

    pics

--- next.config.mjs
+++ next.config.mjs
@@ -6,8 +6,12 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'images.unsplash.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+      },
     ],
   },
 };
 
-export default nextConfig;
+export default nextConfig; 
